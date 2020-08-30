402df0a4ab485351870d29b98fccaefdd04cdfbf
res-dk <53861802+res-dk@users.noreply.github.com>
2020-08-30 22:13:22 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -15,15 +15,15 @@ rm -f alis-recovery.sh
 rm -f alis-recovery-asciinema.sh
 rm -f alis-recovery-reboot.sh
 
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis.conf
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis.sh
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-asciinema.sh
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-reboot.sh
+curl -O https://raw.githubusercontent.com/res-k/alis/master/alis.conf
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis.sh
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-asciinema.sh
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-reboot.sh
 
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.conf
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.sh
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-asciinema.sh
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-reboot.sh
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-recovery.conf
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-recovery.sh
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-recovery-asciinema.sh
+curl -O https://raw.githubusercontent.com/res-dk/alis/master/alis-recovery-reboot.sh
 
 chmod +x alis.sh
 chmod +x alis-asciinema.sh
